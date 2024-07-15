e2de60c108d90b41b825e558befd8fc630c5a1e1
Manojkumar Gopalramanujam <manojkumar.gopalramanujam@msc.com>
2024-07-15 15:11:30 +0530

    Add project files.

---
+++ reactwebapplication.client/src/App.jsx
@@ -0,0 +1,111 @@
+import { useEffect, useState } from 'react';
+import './App.css';
+import { Button } from '@progress/kendo-react-buttons';
+import { ComboBox } from '@progress/kendo-react-dropdowns';
+import '@progress/kendo-theme-default/dist/all.css';
+
+//function App1() {
+//    const [forecasts, setForecasts] = useState();
+
+//    useEffect(() => {
+//        populateWeatherData();
+//    }, []);
+
+//    const contents = forecasts === undefined
+//        ? <p>Loading....done</p>
+//        : <table className="table table-striped" aria-labelledby="tabelLabel">
+//            <thead>
+//                <tr>
+//                    <th>Date</th>
+//                    <th>Temp. (C)</th>
+//                    <th>Temp. (F)</th>
+//                    <th>Summary</th>
+//                </tr>
+//            </thead>
+//            <tbody>
+//                {forecasts.map(forecast =>
+//                    <tr key={forecast.date}>
+//                        <td>{forecast.date}</td>
+//                        <td>{forecast.temperatureC}</td>
+//                        <td>{forecast.temperatureF}</td>
+//                        <td>{forecast.summary}</td>
+//                    </tr>
+//                )}
+//            </tbody>
+//        </table>;
+
+//    return (
+//        <div>
+//            <h1 id="tabelLabel">Hello World App1</h1>
+//            <p>This component demonstrates fetching data from the server.</p>
+//            {contents}
+//        </div>
+//    );
+    
+//    async function populateWeatherData() {
+//        const response = await fetch('weatherforecast');
+//        const data = await response.json();
+//        setForecasts(data);
+//    }
+//}
+function App2() {
+    const [forecasts, setForecasts] = useState();
+
+    useEffect(() => {
+        populateWeatherData();
+    }, []);
+
+    const contents = forecasts === undefined
+        ? <p>Loading....done</p>
+        : <table className="table table-striped" aria-labelledby="tabelLabel">
+            <thead>
+                <tr>
+                    <th>Date</th>
+                    <th>Temp. (C)</th>
+                    <th>Temp. (F)</th>
+                    <th>Summary</th>
+                </tr>
+            </thead>
+            <tbody>
+                {forecasts.map(forecast =>
+                    <tr key={forecast.date}>
+                        <td>{forecast.date}</td>
+                        <td>{forecast.temperatureC}</td>
+                        <td>{forecast.temperatureF}</td>
+                        <td>{forecast.summary}</td>
+                    </tr>
+                )}
+            </tbody>
+        </table>;
+
+    return (
+        <div>
+            <h1 id="tabelLabel">Hello World App2</h1>
+            <p>This component demonstrates fetching data from the server.</p>
+            {contents}
+        </div>
+    );
+    
+    async function populateWeatherData() {
+        const response = await fetch('weatherforecast');
+        const data = await response.json();
+        setForecasts(data);
+    }
+}
+
+
+function App() {
+    return (
+        <div className="App">
+            <Button>Click me</Button>
+            <ComboBox
+                data={['Option 1', 'Option 2', 'Option 3']} // Replace with your data
+                placeholder="Select an option"
+            />
+        </div>
+
+    );
+}
+
+
+export default { App, App2 };
